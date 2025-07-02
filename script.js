6293a04de021b3491bb92bf9d43f7bab4b616526
Maël Auzenet <mauzenet@gmail.com>
2025-07-02 11:07:20 +0200

    hero section

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const lenis = new Lenis({
   autoRaf: true,
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault(); // Empêche le comportement par défaut du lien
+
+      // Récupère l'élément cible (section)
+      const targetId = this.getAttribute('href').substring(1); // Retire le "#"
+      const targetElement = document.getElementById(targetId);
+
+      // Fait défiler la page de manière fluide vers l'élément cible
+      if (targetElement) {
+          targetElement.scrollIntoView({
+              behavior: 'smooth', // Rend le scroll fluide
+              block: 'start' // Aligne la section en haut de l'écran
+          });
+      }
+  });
+});
